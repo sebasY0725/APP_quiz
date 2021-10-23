a830c3ecf611008c55c46b3424fd67fe17d8bdbc
sebasY0725 <86635413+sebasY0725@users.noreply.github.com>
2021-10-23 14:41:51 -0500

    High Scores saved Se guarda los scores de manera local, arreglar el console log fugaz

--- src/end.js
+++ src/end.js
@@ -4,11 +4,27 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
+const MAX_HIGH_SCORES = 5;
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup", () => {
-  saveScoreBtn.disabled = !username.nodeValue;
+  // saveScoreBtn.disabled = !username.nodeValue;
 });
 
-const saveHighScore = (e) => {
+let saveHighScore = (e) => {
   console.log("Saved");
-  e.preventDefault;
+  // e.preventDefault;
+
+  const score = {
+    score: mostRecentScore,
+    name: username.value
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => {
+    return b.score - a.score;
+  });
+  highScores.splice(5);
+  localStorage.setItem("highScores", JSON.stringify(highScores));
 };
+
+saveHighScore();

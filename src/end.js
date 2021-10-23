c6e293ba4bd6eb570b962eadfca986d968284063
sebasY0725 <86635413+sebasY0725@users.noreply.github.com>
2021-10-22 23:39:45 -0500

    Save scores se cambia el score

--- src/end.js
+++ src/end.js
@@ -4,9 +4,9 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 finalScore.innerText = mostRecentScore;
 
-/*username.addEventListener("keyup", () => {
+username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.nodeValue;
-});*/
+});
 
 const saveHighScore = (e) => {
   console.log("Saved");
